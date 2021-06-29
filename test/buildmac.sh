1988b2adcc309c45c47318aa58b02e590d3e9d4e
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2021-06-28 21:25:51 -0400

    Update headers

--- test/buildmac.sh
+++ test/buildmac.sh
@@ -1,4 +1,4 @@
-cython -o ../build/tabularterraform.c --embed  ../source/tabularterraform.py
+cython -o ../build/tabularterraform.c --embed  ../src/tabularterraform.py
 cc -c ../build/tabularterraform.c -o ../build/tabularterraform.o -I/Library/Frameworks/Python.framework/Versions/3.8/include/python3.8 -Wno-unused-result -Wsign-compare -Wunreachable-code -fno-common -dynamic -DNDEBUG -g -fwrapv -O3 -Wall -arch x86_64 -g
 
 cc ../build/tabularterraform.o -o ../build/tabularterraform -L/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/config-3.8-darwin -lpython3.8 -ldl -framework CoreFoundation
